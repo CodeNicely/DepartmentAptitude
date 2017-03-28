6173f287e676378a442c0c20f89189d23e52c129
Meghal Agrawal - Developer <meghal.nit@gmail.com>
2017-03-28 15:49:53 +0530

    Made Admin for all

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from quiz.models import QuizData, QuizSubjectData, QuizQuestionData, QuizResponseData, QuizQuestionResponseData
+
+
+class QuizDataAdmin(admin.ModelAdmin):
+    list_display = []
+
+
+admin.site.register(QuizData, QuizDataAdmin)
+
+
+class QuizSubjectDataAdmin(admin.ModelAdmin):
+    list_display = []
+
+
+admin.site.register(QuizSubjectData, QuizSubjectDataAdmin)
+
+
+class QuizQuestionDataAdmin(admin.ModelAdmin):
+    list_display = []
+
+
+admin.site.register(QuizQuestionData, QuizQuestionDataAdmin)
+
+
+class QuizResponseDataAdmin(admin.ModelAdmin):
+    list_display = []
+
+
+admin.site.register(QuizResponseData, QuizResponseDataAdmin)
+
+
+class QuizQuestionResponseDataAdmin(admin.ModelAdmin):
+    list_display = []
+
+
+admin.site.register(QuizQuestionResponseData, QuizQuestionResponseDataAdmin)
--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from student.models import StudentData, BranchData, SemesterData
+
+
+class StudentDataAdmin(admin.ModelAdmin):
+    list_display = []
+
+
+admin.site.register(StudentData, StudentDataAdmin)
+
+
+class BranchDataAdmin(admin.ModelAdmin):
+    list_display = []
+
+
+admin.site.register(BranchData, BranchDataAdmin)
+
+
+class SemesterDataAdmin(admin.ModelAdmin):
+    list_display = []
+
+
+admin.site.register(SemesterData, SemesterDataAdmin)
